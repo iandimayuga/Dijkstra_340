e4dcaa1a7759c4021e47f7416124c4c8f893cd06
icd3 <git@iandimayuga.com>
2010-11-21 15:10:10 -0500

    Added some documentation to the print function so you know how to use it. Sorry for the small commits, trying to learn how to use git properly.

--- src/imo/Display.java
+++ src/imo/Display.java
@@ -9,11 +9,12 @@ public class Display
 	
 	/**
 	 * Displays two graphs, side by side.
-	 * @param g
-	 * @param vHigh
-	 * @param eHigh
+	 * @param g1 The first graph, displayed in traditional layout
+	 * @param g2 The second graph, displayed as a tree.
+	 * @param vHigh A set of vertices you'd like highlighted in a color different from both "finalized" and "unfinalized."
+	 * @param eHigh A set of edges you'd like highlighted. Good for "best path" highlighting.
 	 */
-	public void print(Graph g1, Graph g2, LinkedList<Vertex> vHigh, LinkedList<Edge> eHigh)
+	public void print( Graph g1, Graph g2, LinkedList<Vertex> vHigh, LinkedList<Edge> eHigh)
 	{
 		
 	}

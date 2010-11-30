284468bedc2ce12609d337954e5b481994a21e0a
icd3 <git@iandimayuga.com>
2010-11-30 13:54:10 -0500

    added capability to disable display using a static variable, for the purposes of runtime analysis

--- src/imo/Display.java
+++ src/imo/Display.java
@@ -24,6 +24,8 @@ import edu.uci.ics.jung.visualization.renderers.Renderer.VertexLabel.Position;
 
 public final class Display
 {
+	public static boolean DISPLAY = true;
+	
 	private static JFrame frame;
 	private static Layout<Vertex, Edge> graph;
 	private static Layout<Vertex, Edge> tree;
@@ -32,70 +34,71 @@ public final class Display
 	
 	public Display( Graph<Vertex, Edge> g, Forest<Vertex, Edge> f, final Collection<Vertex> vHigh, final Collection<Edge> eHigh)
 	{
-		graph = new ISOMLayout<Vertex, Edge>( g);
-		Transformer<Vertex, Paint> vPaint = new Transformer<Vertex, Paint>()
-		{
-			public Paint transform( Vertex v)
+		if( DISPLAY) {
+			graph = new ISOMLayout<Vertex, Edge>( g);
+			Transformer<Vertex, Paint> vPaint = new Transformer<Vertex, Paint>()
 			{
-				if( vHigh.contains( v)) {
-					return Color.yellow;
-				} else if( v.isFinal()) {
-					return Color.green;
-				} else {
-					return Color.red;
+				public Paint transform( Vertex v)
+				{
+					if( vHigh.contains( v)) {
+						return Color.yellow;
+					} else if( v.isFinal()) {
+						return Color.green;
+					} else {
+						return Color.red;
+					}
 				}
-			}
-		};
-		
-		Transformer<Edge, Paint> ePaint = new Transformer<Edge, Paint>()
-		{
-			public Paint transform( Edge e)
+			};
+			
+			Transformer<Edge, Paint> ePaint = new Transformer<Edge, Paint>()
 			{
-				return eHigh.contains( e) ? Color.red : Color.black;
+				public Paint transform( Edge e)
+				{
+					return eHigh.contains( e) ? Color.red : Color.black;
+				}
+			};
+			
+			EdgeLabelRenderer eRend = new DefaultEdgeLabelRenderer( Color.red, false);
+			
+			// left side
+			try {
+				graph.setSize( new Dimension( 500, 500));
+			} catch( NullPointerException e) {
+				System.out.println( "Display has not been initialized");
+				System.exit( -1);
 			}
-		};
-		
-		EdgeLabelRenderer eRend = new DefaultEdgeLabelRenderer( Color.red, false);
-		
-		// left side
-		try {
-			graph.setSize( new Dimension( 500, 500));
-		} catch( NullPointerException e) {
-			System.out.println( "Display has not been initialized");
-			System.exit( -1);
+			
+			left = new BasicVisualizationServer<Vertex, Edge>( graph);
+			left.setPreferredSize( new Dimension( 600, 600));
+			
+			left.getRenderContext().setVertexFillPaintTransformer( vPaint);
+			left.getRenderContext().setEdgeDrawPaintTransformer( ePaint);
+			left.getRenderContext().setVertexLabelTransformer( new ToStringLabeller());
+			left.getRenderContext().setEdgeLabelTransformer( new ToStringLabeller());
+			left.getRenderContext().setEdgeLabelRenderer( eRend);
+			left.getRenderer().getVertexLabelRenderer().setPosition( Position.CNTR);
+			
+			// right side
+			if( f != null) {
+				tree = new TreeLayout<Vertex, Edge>( f);
+				
+				right = new BasicVisualizationServer<Vertex, Edge>( tree);
+				right.setPreferredSize( new Dimension( 600, 600));
+				
+				right.getRenderContext().setVertexFillPaintTransformer( vPaint);
+				right.getRenderContext().setEdgeDrawPaintTransformer( ePaint);
+				right.getRenderContext().setVertexLabelTransformer( new ToStringLabeller());
+				right.getRenderContext().setEdgeLabelTransformer( new ToStringLabeller());
+				right.getRenderContext().setEdgeLabelRenderer( eRend);
+				right.getRenderer().getVertexLabelRenderer().setPosition( Position.CNTR);
+				
+				right.setLocation( 600, 0);
+			}
+			
+			left.setLocation( 0, 0);
+			
+			frame = new JFrame( "Dijkstra's Algorithm");
 		}
-		
-		left = new BasicVisualizationServer<Vertex, Edge>( graph);
-		left.setPreferredSize( new Dimension( 600, 600));
-		
-		left.getRenderContext().setVertexFillPaintTransformer( vPaint);
-		left.getRenderContext().setEdgeDrawPaintTransformer( ePaint);
-		left.getRenderContext().setVertexLabelTransformer( new ToStringLabeller());
-		left.getRenderContext().setEdgeLabelTransformer( new ToStringLabeller());
-		left.getRenderContext().setEdgeLabelRenderer( eRend);
-		left.getRenderer().getVertexLabelRenderer().setPosition( Position.CNTR);
-		
-		// right side
-		if( f != null) {
-			tree = new TreeLayout<Vertex, Edge>( f);
-			
-			right = new BasicVisualizationServer<Vertex, Edge>( tree);
-			right.setPreferredSize( new Dimension( 600, 600));
-			
-			right.getRenderContext().setVertexFillPaintTransformer( vPaint);
-			right.getRenderContext().setEdgeDrawPaintTransformer( ePaint);
-			right.getRenderContext().setVertexLabelTransformer( new ToStringLabeller());
-			right.getRenderContext().setEdgeLabelTransformer( new ToStringLabeller());
-			right.getRenderContext().setEdgeLabelRenderer( eRend);
-			right.getRenderer().getVertexLabelRenderer().setPosition( Position.CNTR);
-			
-			right.setLocation( 600, 0);
-		}
-		
-		left.setLocation( 0, 0);
-		
-		frame = new JFrame( "Dijkstra's Algorithm");
-		
 	}
 	
 	/**
@@ -109,16 +112,18 @@ public final class Display
 	@SuppressWarnings( { "rawtypes", "unchecked" })
 	public void print()
 	{
-		left.fireStateChanged();
-		if( right != null) right.fireStateChanged();
-		
-		// start frame
-		JPanel panel = new JPanel();
-		panel.add( left);
-		if( right != null) panel.add( right);
-		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add( panel);
-		frame.pack();
-		frame.setVisible( true);
+		if( DISPLAY) {
+			left.fireStateChanged();
+			if( right != null) right.fireStateChanged();
+			
+			// start frame
+			JPanel panel = new JPanel();
+			panel.add( left);
+			if( right != null) panel.add( right);
+			frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+			frame.getContentPane().add( panel);
+			frame.pack();
+			frame.setVisible( true);
+		}
 	}
 }
